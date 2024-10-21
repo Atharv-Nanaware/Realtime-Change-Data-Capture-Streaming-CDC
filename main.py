8db0107d73af261eb13f053120caa9b9cb5e1a40
Atharv_Nanaware <atharvnanaware01@gmail.com>
2024-10-21 23:39:48 +0530

    Updating project files

--- main.py
+++ main.py
@@ -3,26 +3,28 @@ import psycopg2
 from datetime import datetime
 import random
 
-
-fake = faker.Faker()
+fake= faker.Faker()
 
 def generate_transaction():
-    user = fake.simple_profile()
+    user=fake.simple_profile()
 
     return {
-        "transactionId": fake.uuid4(),
-        "userId": user['username'],
+        "transactionId" : fake.uuid4(),
+        "userId":user['username'],
         "timestamp": datetime.utcnow().timestamp(),
-        "amount": round(random.uniform(10, 1000), 2),
-        "currency": random.choice(['USD', 'GBP']),
-        'city': fake.city(),
-        "country": fake.country(),
-        "merchantName": fake.company(),
+        "amount": round(random.uniform(10,1000),2),
+        "currency": random.choice(['USD',"GBP"]),
+        "city":fake.city(),
+        "country":fake.country(),
+        "merchantName":fake.company(),
         "paymentMethod": random.choice(['credit_card', 'debit_card', 'online_transfer']),
-        "ipAddress": fake.ipv4(),
-        "voucherCode": random.choice(['', 'DISCOUNT10', '']),
-        'affiliateId': fake.uuid4()
+        "ipAddress":random.choice(['', 'DISCOUNT10', '']),
+        "affiliateId":fake.uuid4(),
+        "voucher_code": fake.word()  #  this line  added to generate a voucher code
     }
+
+
+
 
 def create_table(conn):
     cursor = conn.cursor()
@@ -71,7 +73,7 @@ if __name__ == "__main__":
         """, (transaction["transactionId"], transaction["userId"], datetime.fromtimestamp(transaction["timestamp"]).strftime('%Y-%m-%d %H:%M:%S'),
               transaction["amount"], transaction["currency"], transaction["city"], transaction["country"],
               transaction["merchantName"], transaction["paymentMethod"], transaction["ipAddress"],
-              transaction["affiliateId"], transaction["voucherCode"])
+              transaction["affiliateId"], transaction["voucher_code"])
     )
 
     cur.close()
